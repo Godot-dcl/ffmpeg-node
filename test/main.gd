9d7091309d88db7f2f8fae66c100e9f696ef6e14
Michael Alexsander <michaelalexsander@protonmail.com>
2022-02-18 19:13:30 -0300

    update 15

---
+++ test/main.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+const FILE_PATH =\
+		"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+const TIME_SKIP = 5
+
+@onready var ffmpeg = $FFmpegRect
+
+
+func _ready():
+	ffmpeg.load(FILE_PATH)
+	ffmpeg.play()
+	ffmpeg.set_loop(true)
+
+
+func _input(event):
+	if event.is_action_pressed("ui_left"):
+		ffmpeg.seek(ffmpeg.get_playback_position() - TIME_SKIP)
+	elif event.is_action_pressed("ui_right"):
+		ffmpeg.seek(ffmpeg.get_playback_position() + TIME_SKIP)
+	elif event.is_action_pressed("ui_accept"):
+		ffmpeg.set_paused(not ffmpeg.is_paused())
